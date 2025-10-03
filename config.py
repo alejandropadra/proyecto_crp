44fe3d75c6959ec3c2e738b0d56bf7bc2a76265a
Alejandro Padra <digitalingpadra@gmail.com>
2025-10-03 13:32:36 +0000

    sccpy

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-#local= 'mysql://root:Di.IoT4.0@localhost/cobranza'
-local= 'mysql://Di:DiIoT4.0@localhost/app_crp'
-server = 'mysql://apadra:Di.2824@localhost/app_crp'
-
 class Config:
     SECRET_KEY = 'codigofacilito'
 
@@ -12,10 +8,7 @@ class DevelopmentConfig(Config):
     MAIL_SERVER = 'smtp.office365.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME =  'noreply@corimon.com' #'alejandro_padra@corimon.com'
-    MAIL_PASSWORD =  'yxjtrjppvwgjqmld' #'flqlgksndjffxyqn' 
-    #MAIL_USERNAME = 'noreply@corimon.com'
-    #MAIL_PASSWORD = 'gnvndybbntwqhbvp'
+    
     MAIL_TEST = ''
 
 class ProductionConfig(DevelopmentConfig):
